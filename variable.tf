cc32d01e4c95061ec5de866a9c8fe83e7a76f1f5
priti620 <121668165+priti620@users.noreply.github.com>
2023-07-31 11:39:21 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -5,17 +5,17 @@ variable "gcp_project" {
 
 variable "gcp_region" {
   description = "GCP region, e.g. us-east1"
-  default     = "us-east1"
+  default     = "us-central1"
 }
 
 variable "gcp_zone" {
   description = "GCP zone, e.g. us-east1-a"
-  default     = "us-east1-a"
+  default     = "us-central1-a"
 }
 
 variable "machine_type" {
   description = "GCP machine type"
-  default     = "n1-standard-1"
+  default     = "e2-medium"
 }
 
 variable "instance_name" {
